59a2d9a12673bdec9dd6ce9d5186a045b32ffe65
kuldeep <thekuldeep2611@gmail.com>
2020-04-27 12:19:58 +0530

    changed implementaion of hash table

--- Data Structures - Hashtables/Hash Table Implementation.py
+++ Data Structures - Hashtables/Hash Table Implementation.py
@@ -1,42 +1,46 @@
-from random import randint
-class Hashtable():
-  def __init__(self):
-    self.mydict = ['None']*50
-    self.addr_list = []
+class Hashmap():
+     def __init__(self):
+          self.size=50
+          self.map=[None]*self.size
+
+     def _gethash(self,key):
+          h=0
+          for char in str(key):
+               h+=ord(char)
+          return h % self.size
+     def set(self,key,value):
+          address=self._gethash(key) 
+          if self.map[address]==None:
+               self.map[address]=[]
+          self.map[address].append([key,value])     
+
+          return self.map
+     def get(self,key):
+          address=self._gethash(key)
+          currentbucket=self.map[address]
+          if currentbucket:
+               for i in range(len(currentbucket)):
+                    if currentbucket[i][0]==key:
+                         return currentbucket[i][1]
+          else:
+               return 'undefined'
+     def keys(self):
+          keysarray=[]
+          for i in range(len(self.map)):
+               if self.map[i]:
+                    print (self.map[i])
+                    keysarray.append(self.map[i][0][0])
+          return keysarray          
+
+myhash=Hashmap()
+myhash.set('grapes',52)
+print(myhash.get('grapes'))
+print(myhash.keys())
+
+
+        
+
+
   
-  def __str__(self):
-    return str(self.__dict__)
 
-  def _hash(self):
-    while True:
-      x = randint(0,49)
-      if x not in self.addr_list:
-        return x
-  
-  def set(self,key,value):
-    address = self._hash()
-    self.mydict[address] = [key,value]
-    self.addr_list.append(address)
-
-  def get(self,key):
-    
-    for i in self.addr_list:
-      if self.mydict[i][0] == key:
-        return self.mydict[i][1]    
-
-  def keys(self):
-    key_arr=[]
-    for i in self.addr_list:
-      key_arr.append(self.mydict[i][0])
-    return key_arr
-
-h=Hashtable()
-h.set('grapes',1000)
-h.set('apples',10)
-h.set('oranges',300)
-h.set('bananas',200)
-x=h.get('grapes')
-key_arr = h.keys()
-print(h)
-print(x)
-print(key_arr)
+                               
