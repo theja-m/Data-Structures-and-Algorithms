6cbb6aff32c7452244eee6fcad086446d7c6d8c2
matt985 <55977069+matt985@users.noreply.github.com>
2020-05-05 10:55:40 +0200

    Update Implementing an array.py
    Added a get method to access elements in MyArray by their index

--- Data Structures - Arrays/Implementing an array.py
+++ Data Structures - Arrays/Implementing an array.py
@@ -6,6 +6,9 @@ class MyArray:
   def __str__(self):
     return str(self.__dict__)
 
+  def get(self,index):
+    return self.data[index]
+  
   def push(self,item):
     self.data[self.length]=item
     self.length+=1
