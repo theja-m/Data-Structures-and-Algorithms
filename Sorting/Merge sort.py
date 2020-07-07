52b621a5a569cc7303b3510a6812b9aa27bde288
wiiilcome <41098506+wiiilcome@users.noreply.github.com>
2020-07-07 15:26:07 -0400

    Update

--- Sorting/Merge sort.py
+++ Sorting/Merge sort.py
@@ -1,32 +1,86 @@
-arr = [99,44,6,2,1,5,63,87,283,4,0]
+def mergesort(array):
+    lng = len(array)
 
-def mergesort(arr):
-  if len(arr) == 1:
-    return arr
-  length = len(arr)
-  mid = length // 2
-  left = arr[:mid]
-  right = arr[mid:]
-  print('Left {}'.format(left))
-  print('Right {}'.format(right))
+    if lng == 1:
+        return array
 
-  return merge(mergesort(left),mergesort(right))
+    # Split Array in into right and left
+    mid = lng // 2
+    left = array[0:mid]
+    right = array[mid:]
 
-def merge(left,right):
-  result = []
-  leftindex = 0
-  rightindex = 0
-  while leftindex < len(left) and rightindex < len(right):
-    if left[leftindex] < right[rightindex]:
-      result.append(left[leftindex])
-      leftindex += 1
-    else:
-      result.append(right[rightindex])
-      rightindex += 1
-  print(left,right)
-  print( result + left[leftindex:] + right[rightindex:] )
-  return result + left[leftindex:] + right[rightindex:]
+    # print("left:",left)
+    # print("right:",right)
+
+    return merge(mergesort(left), mergesort(right))
 
 
-x = mergesort(arr)
-print(x)
+def merge(left, right):
+    sort = []
+    l_indx = 0
+    r_indx = 0
+
+    while (l_indx < len(left)) & (r_indx < len(right)):
+        if left[l_indx] < right[r_indx]:
+            sort.append(left[l_indx])
+            l_indx += 1
+        else:
+            sort.append(right[r_indx])
+            r_indx += 1
+
+    sort = sort + left[l_indx:] + right[r_indx:]
+
+    print(sort)
+
+    return sort
+
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+mergesort(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# solution:
+
+# arr = [99,44,6,2,1,5,63,87,283,4,0]
+
+# def mergesort(arr):
+#   if len(arr) == 1:
+#     return arr
+#   length = len(arr)
+#   mid = length // 2
+#   left = arr[:mid]
+#   right = arr[mid:]
+#   print('Left {}'.format(left))
+#   print('Right {}'.format(right))
+
+#   return merge(mergesort(left),mergesort(right))
+
+# def merge(left,right):
+#   result = []
+#   leftindex = 0
+#   rightindex = 0
+#   while leftindex < len(left) and rightindex < len(right):
+#     if left[leftindex] < right[rightindex]:
+#       result.append(left[leftindex])
+#       leftindex += 1
+#     else:
+#       result.append(right[rightindex])
+#       rightindex += 1
+#   print(left,right)
+#   print( result + left[leftindex:] + right[rightindex:] )
+#   return result + left[leftindex:] + right[rightindex:]
+
+
+# x = mergesort(arr)
+# print(x)
---
+++ Sorting/Quick sort.py
@@ -0,0 +1,23 @@
+def quicksort(array,left,right):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+# Select first and last index as 2nd and 3rd parameters
+quicksort(numbers,0,len(numbers)-1)
