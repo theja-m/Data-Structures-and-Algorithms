275aeb0bd0301eb5240537d2157dd54abacfebff
Vanshaj Garg <43954654+vanshaj26@users.noreply.github.com>
2020-08-05 03:11:37 +0530

    Update Hash Table Implementation.py
    It make it dymanic, we can define size of dictionary

--- Data Structures - Hashtables/Hash Table Implementation.py
+++ Data Structures - Hashtables/Hash Table Implementation.py
@@ -1,42 +1,49 @@
 from random import randint
-class Hashtable():
-  def __init__(self):
-    self.mydict = ['None']*50
-    self.addr_list = []
-  
-  def __str__(self):
-    return str(self.__dict__)
 
-  def _hash(self):
-    while True:
-      x = randint(0,49)
-      if x not in self.addr_list:
-        return x
-  
-  def set(self,key,value):
-    address = self._hash()
-    self.mydict[address] = [key,value]
-    self.addr_list.append(address)
+class hash_lis:
 
-  def get(self,key):
-    
-    for i in self.addr_list:
-      if self.mydict[i][0] == key:
-        return self.mydict[i][1]    
+    def __init__(self,size):
+        self.size = size
+        self.mydict = ['None']*self.size
+        self.add_list = []
 
-  def keys(self):
-    key_arr=[]
-    for i in self.addr_list:
-      key_arr.append(self.mydict[i][0])
-    return key_arr
+    def __str__(self):
+        return str(self.__dict__)
 
-h=Hashtable()
-h.set('grapes',1000)
-h.set('apples',10)
-h.set('oranges',300)
-h.set('bananas',200)
-x=h.get('grapes')
-key_arr = h.keys()
-print(h)
+    def _hash(self):
+        while True:
+            x = randint(0,self.size-1)
+            if x not in self.add_list:
+                return x
+
+    def _set(self,key,value):
+        add = self._hash()
+        self.mydict[add] = [key,value]
+        self.add_list.append(add)
+
+    def _get(self,key):
+        for i in self.add_list:
+            if self.mydict[i][0] == key:
+                return self.mydict[i][1]
+
+    def _key(self):
+        key_arr = []
+        for i in self.add_list:
+            key_arr.append(self.mydict[i][0])
+        return key_arr 
+
+
+h = hash_lis(3)
+h._set('grapes',1000)
+h._set('lemon',20)
+h._set('grap',100)
+
+x=h._get('grapes')
 print(x)
+x=h._get('lemon')
+print(x)
+x=h._get('grap')
+print(x)
+key_arr = h._key()
+print(h)
 print(key_arr)
