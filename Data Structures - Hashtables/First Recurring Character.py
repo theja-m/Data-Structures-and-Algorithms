6c46c254ecab7629ca481b771220467399dd3525
Hyang0219 <yanghl0219@gmail.com>
2020-08-20 20:38:08 +0100

    modified iterative function for first recurring number exercise

--- Data Structures - Hashtables/First Recurring Character.py
+++ Data Structures - Hashtables/First Recurring Character.py
@@ -1,14 +1,19 @@
-def func(mylist):
+def func_iter(mylist):
 
-  for i in range(0,len(mylist)):
+  for i in range(len(mylist)):
+    temp = []
     for j in range(i+1,len(mylist)):
+      if mylist[j] in temp:
+        return mylist[j]
+      else:
+        temp.append(mylist[j])
       if mylist[i] == mylist[j]:
         return mylist[i] 
   return 0
 
 def hashtable(mylist):
   mydict = {}
-  for i in range(0,len(mylist)):
+  for i in range(len(mylist)):
     if mylist[i] in mydict:
       return mylist[i]
     else:
@@ -19,3 +24,5 @@ def hashtable(mylist):
 mylist = [2,1,1,2,3,4,5]
 x = hashtable(mylist)
 print(x)
+y = func_iter(mylist)
+print(y)
