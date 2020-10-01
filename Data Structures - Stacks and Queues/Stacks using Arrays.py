b444886e36b21f8aff2fad7d33775143e8e737b0
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2020-10-01 14:06:54 +0530

    Update Stacks using Arrays.py
    This new code shows element of stack in a better format.
    It also has size and isEmpty method to check size of stack or stack is empty or not at any point of time.

--- Data Structures - Stacks and Queues/Stacks using Arrays.py
+++ Data Structures - Stacks and Queues/Stacks using Arrays.py
@@ -3,16 +3,31 @@ class Stack:
     self.arr = []
     self.length = 0
   
+  # return stack 
   def __str__(self):
-    return str(self.__dict__)
+    return str(self.arr)
   
+  # return length of stack
+  def size(self):
+    return self.length
+
+  # return True if stack is Empty otherwise return False
+  def isEmpty(self):
+    if self.length==0:
+        return True
+    else:
+        return False
+        
+  # return top of stack
   def peek(self):
     return self.arr[self.length-1]
 
+  # push value in stack
   def push(self,value):
     self.arr.append(value)
     self.length += 1
 
+  # return top of stack and also remove it from  stack
   def pop(self):
     popped_item = self.arr[self.length-1]
     del self.arr[self.length-1]
@@ -29,5 +44,5 @@ x = mystack.peek()
 print(x)
 mystack.pop()
 print(mystack)
-
-
+print(mystack.isEmpty())
+print(mystack.size())
